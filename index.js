5c624e1e85fb33528bb486111508bf7e959ba67c
eli <eli.fitch.prof@gmail.com>
2016-02-29 09:18:20 -0500

    trim

--- index.js
+++ index.js
@@ -4,8 +4,5 @@ let watchify = require('./github-watchify');
 let env = require('dotenv').config();
 
 watchify.authenticate(
-  process.env.CLIENT_ID, 
-  process.env.CLIENT_SECRET, 
-  process.env.GITHUB_USER, 
   process.env.GITHUB_TOKEN
 );
